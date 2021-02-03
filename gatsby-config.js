b4ad0bb368d773d8b2b174c0db61d92bc85b45cc
BFlorian91 <b.florian91@icloud.com>
2021-02-03 11:52:19 +0100

    google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -63,7 +63,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `G-GHV8B4RZZP`,
       },
     },
     {
