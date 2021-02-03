4c0394cf2b94e9ac5386583ce9a4089764e53ec7
BFlorian91 <b.florian91@icloud.com>
2021-02-03 14:40:39 +0100

    google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -61,9 +61,24 @@ module.exports = {
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
-      resolve: `gatsby-plugin-google-analytics`,
+      resolve: `gatsby-plugin-google-gtag`,
       options: {
-        trackingId: `G-GHV8B4RZZP`,
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "G-MHHGL5C9HD", // Google Analytics / GA
+        ],
+        // This object gets passed directly to the gtag config command
+        // This config will be shared across all trackingIds
+        gtagConfig: {
+          optimize_id: "OPT_CONTAINER_ID",
+          anonymize_ip: true,
+          cookie_expires: 0,
+        },
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: true,
+        },
       },
     },
     {
@@ -129,8 +144,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Gatsby Starter Blog`,
-        short_name: `GatsbyJS`,
+        name: `Le Blog d'un dev`,
+        short_name: `LBDD`,
         start_url: `/`,
         background_color: `#ffffff`,
         theme_color: `#663399`,
